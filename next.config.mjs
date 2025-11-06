8219be9622d154e07822a4fb4dea753e1f9d1214
Alex Matheson <alex.kmatheson@gmail.com>
2025-11-06 10:01:30 -0700

    Add redirect for resume to external PDF link

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,15 @@ import createMDX from '@next/mdx';
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  async redirects() {
+    return [
+      {
+        source: '/resume',
+        destination: 'https://job.alexkev.com/Alex_Matheson_Resume.pdf',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 const withMDX = createMDX({
